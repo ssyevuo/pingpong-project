6ae3ea6f31f88d24226dfa951c85b3b2f9c120c5
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-03-10 23:07:21 +0300

    Updated utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
-# necessary importants
+# import necessary files
 import pygame
 from constants import *
 
 # reset ball function
 def reset_ball(ball):
+    # moves the ball to the center of the screen thats where it starts
     ball.rect.x = WIDTH // 2 - BALL_SIZE // 2
     ball.rect.y = HEIGHT // 2 - BALL_SIZE // 2
+    
     ball.speed_x = BALL_SPEED_X * (-1 if ball.speed_x < 0 else 1) # reset ball direction
     ball.speed_y = BALL_SPEED_Y # reset y speed.
 
